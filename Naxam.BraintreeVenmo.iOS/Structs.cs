a5b86406303865fc877b604148008dd31868848c
Vu Duc Tuyen <tuyen@naxam.net>
2017-05-28 13:46:07 +0700

    Braintree iOS SDK v4.8.2 - Venmo

---
+++ Naxam.BraintreeVenmo.iOS/Structs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ObjCRuntime;
+
+namespace BraintreeVenmo
+{
+	[Native]
+	public enum BTVenmoDriverErrorType : long
+	{
+		Unknown = 0,
+		Disabled,
+		AppNotAvailable,
+		BundleDisplayNameMissing,
+		AppSwitchFailed,
+		InvalidReturnURL,
+		Integration,
+		InvalidRequestURL
+	}
+}
