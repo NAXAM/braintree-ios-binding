0105f39a96fe342bd406a07ec6aca5e69b810096
Vu Duc Tuyen <tuyen@naxam.net>
2017-07-10 22:56:19 +0700

    Add linker for Card payments

---
+++ Naxam.BraintreeUnionPay.iOS/Linker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Naxam.BraintreeUnionPay.iOS
+{
+    [Foundation.Preserve(AllMembers = true)]
+    public static class Linker
+    {
+        public static void Preserve () {
+            new BraintreeCard.BTCard();
+        }
+    }
+}
