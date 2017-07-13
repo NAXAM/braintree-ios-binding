52b8f6891b1264849a0f9187767e27cd3f6a6ee9
Vu Duc Tuyen <tuyen@naxam.net>
2017-07-13 14:41:37 +0700

    #1-Framework not loaded: Paypal frameworks

---
+++ Naxam.BraintreePayPal.iOS/LinkWith.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+
+namespace BraintreePayPal
+{
+    [Preserve(AllMembers = true)]
+    public static class LinkWith
+    {
+        /// <summary>
+        /// PayPalUtils and PayPalDataCollector are in use internally by BrainTreePayPal.
+        /// BUT from public APIs, Xamarin.iOS couldn't know that.
+        /// This code is to tell Xamarin.iOS that we need those both frameworks.
+        /// </summary>
+        public static void Init() {
+            new PayPalUtils.PPOTDevice();
+            new PayPalDataCollector.PPDataCollector();
+            new PayPalOneTouch.PPOTCore();
+        }
+    }
+}
